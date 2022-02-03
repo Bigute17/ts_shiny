15f2eea33ccbbb852a170ad490efece4e01bbf13
Blake Curtsinger <ccurtsin@vols.utk.edu>
2022-02-03 09:14:50 -0500

    Prototype App
    Minimal Viable Product

--- server.R
+++ server.R
@@ -6,5 +6,17 @@ sales_df$date <- as.Date(sales_df$date)
 sales_df <- as_tsibble(sales_df, key = c("scode", "pcode"), index = "date")
 
 server <- function(input, output) {
+output$debug <- renderPrint({
+  input$selected_product
+  input$selected_store
 
+
+  sales_df[sales_df$scode == input$selected_store & sales_df$pcode == input$selected_product,]
+  })
+output$ts_plot <- renderPlot({
+  plot_df <-  sales_df[sales_df$scode == input$selected_store & sales_df$pcode == input$selected_product,]
+
+  autoplot(plot_df)
+})
 }
+
--- ui.R
+++ ui.R
@@ -1,5 +1,11 @@
 library(shiny)
 
 ui <- fluidPage(
+  selectInput(inputId = "selected_store", label = "Select store:", choices = unique(sales_df$scode)),
+  selectInput(inputId = "selected_product", label = "Select product:", choices = unique(sales_df$pcode)),
+  plotOutput('ts_plot'),
+  verbatimTextOutput("debug"),
+
+
 
 )
